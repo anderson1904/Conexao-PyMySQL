dcdd299b39d2b07088e761f73e3d954068f8f267
anderson1904 <andersoncosta190406@gmail.com>
2024-12-05 15:52:11 -0300

    commit dia 5/12/2024 15:52

--- 5-12-2024/conexao.py
+++ 5-12-2024/conexao.py
@@ -1,12 +1,13 @@
 import pymysql
 
 conexao = pymysql.connect(
-#onde irar rodar o banco de dados, caso não dê tente 127.0.0.1
-    host= 'localhost',
+#onde irar rodar o banco de dados,
+    host= 'localhost',# caso não dê tente 127.0.0.1
 #o nome de usuário do host e sua senha
     user='root',
     password='',
 #por padrão o usuário é 'root' e a senha é nula(não possui senha)
 )
-
-cursor = conexao.cursor()
+cursor = conexao.cursor()#cria um cursor para a conexão com o mysql
+#caso não ocorra erro, essa mensagem aparecerá
+print('Conexão realizada com sucesso')
